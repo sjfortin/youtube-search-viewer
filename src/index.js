94ab773f6067b2adf336d73df4cacfd0e79143a8
Sam Fortin <sam.j.fortin@gmail.com>
2017-11-08 23:19:37 -0600

    Props on VideoList

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+// import API_KEY from './components/api_key';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-import API_KEY from "./components/api_key";
-import SearchBar from "./components/search_bar";
+const API_KEY = 'AIzaSyCrtQ9yVPdIn7D_OxoihLe_Jp1ideWUfs4';
 
-const App = () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
     return (
-        <div>
-            <SearchBar />
-        </div>
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
     );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
