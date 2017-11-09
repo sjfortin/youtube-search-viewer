deb2e98e1a4507b08b67814e8344998d0aeccf3f
Sam Fortin <sam.j.fortin@gmail.com>
2017-11-09 09:43:03 -0600

    VideoListItem rendering image and heading

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
       this.setState({ videos });
     });
   }
