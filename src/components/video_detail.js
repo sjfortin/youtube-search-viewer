f6e0145f776f269a97243c6b4a519bff83124d21
Sam Fortin <sam.j.fortin@gmail.com>
2017-11-09 10:02:11 -0600

    Video title and desc fix

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -11,10 +11,10 @@ const VideoDetail = ({ video }) => {
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url} />
-        <div className="details">
-          <div>{video.snippet.title}</div>
-          <div>{video.snippet.description}</div>
-        </div>
+      </div>
+      <div className="details">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
       </div>
     </div>
   );
