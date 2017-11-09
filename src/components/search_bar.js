e002df82b94f5f19a1eca9ca25e5ae1e62cea071
Sam Fortin <sam.j.fortin@gmail.com>
2017-11-08 20:46:18 -0600

    State and controlled components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,11 +3,17 @@ import React, { Component } from "react"
 class SearchBar extends Component {
     constructor(props) {
         super(props)
-        this.state = { term: ''};
+        this.state = { term: '' };
     }
 
     render() {
-        return <input onChange={event => console.log(event.target.value)} />;
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+            </div>
+        );
     }
 
 }
