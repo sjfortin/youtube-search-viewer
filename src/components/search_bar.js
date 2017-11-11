56e292c06de6d0c7d261f522872a4b836e347e7a
Sam Fortin <sam.j.fortin@gmail.com>
2017-11-10 22:43:20 -0600

    Style update

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { term: '' };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
 
-    render() {
-        return (
-            <div>
-                <input
-                    value={this.state.term}
-                    onChange={event => this.setState({ term: event.target.value })} />
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
